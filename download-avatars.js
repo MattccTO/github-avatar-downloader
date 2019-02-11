ab67e10b4e1f853c08c56453b2d4e1e53f58b78b
Matt Cook <mcc1985@gmail.com>
2019-02-11 21:27:28 +0000

    Update getRepoContributors function with url info and user agent/authorization headers

--- download-avatars.js
+++ download-avatars.js
@@ -2,10 +2,21 @@ const request = require('request');
 
 const fs = require('fs');
 
+const secret = require('./secrets.js');
+
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, callback) {
-
+  let options = {
+    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${secret.GITHUB_TOKEN}`
+    }
+  };
+  request(options, function(err, response, body) {
+    callback(err, body);
+  });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
