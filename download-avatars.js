d8b95c2f5fe688c6ec462bd9cffceadb67b9d270
Matt Cook <mcc1985@gmail.com>
2019-02-11 21:37:28 +0000

    getRepoContributors pulls data from github api and converts to an array of objects
    Callback function logs each avatar URL

--- download-avatars.js
+++ download-avatars.js
@@ -15,11 +15,14 @@ function getRepoContributors(repoOwner, repoName, callback) {
     }
   };
   request(options, function(err, response, body) {
-    callback(err, body);
+    parsedBody = JSON.parse(body);
+    callback(err, parsedBody);
   });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log('Errors: ', err);
-  console.log('Result: ', result);
+  result.forEach(function (result) {
+      console.log('Avatar URL: ', result.avatar_url);
+  });
 });
