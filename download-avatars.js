1256aa58cb3ba56392a9221edf364fcd8bed1ff8
Matt Cook <mcc1985@gmail.com>
2019-02-11 23:46:24 +0000

    Implement the downloadImageByURL function to download images and append proper file extension. Also connected it to function that pulls from repo.
    Updated .gitignore to skip avatars subdir

--- download-avatars.js
+++ download-avatars.js
@@ -6,6 +6,7 @@ const secret = require('./secrets.js');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+// Get the repo contributor info from source
 function getRepoContributors(repoOwner, repoName, callback) {
   let options = {
     url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
@@ -20,9 +21,37 @@ function getRepoContributors(repoOwner, repoName, callback) {
   });
 }
 
+// Downloads the image and applies the correct file extension. I pulled the
+// file extension from the header content type but the asynch nature of the
+// program wasn't adding correctly. Added a rename function on finish but it
+// was buggy. Mentor Ramses helped me find the renameSync function which worked.
+
+function downloadImageByURL(url, filePath) {
+  let fileExtension = '';
+  request.get(url)
+         .on('error', function(err) {
+           console.log('Oopsie, there was an error!');
+           throw err;
+         })
+         .on('response', function(response) {
+           fileExtension = response.headers['content-type'].slice(6);
+           // console.log(fileExtension);
+           console.log(`Response Status Code: ${response.statusCode}`);
+         })
+         .pipe(fs.createWriteStream(filePath))
+         .on('finish', function () {
+           fs.renameSync(filePath, `${filePath}.${fileExtension}`)
+         });
+}
+
+// Invoke the getRepoContributors function
 getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log('Errors: ', err);
+  console.log('Now downloading avatars...');
   result.forEach(function (result) {
-      console.log('Avatar URL: ', result.avatar_url);
+    let currentUserID = result.login;
+    let currentURL = result.avatar_url;
+    let currentFilePath = `./avatars/${currentUserID}`;
+    downloadImageByURL(currentURL, currentFilePath);
   });
 });
